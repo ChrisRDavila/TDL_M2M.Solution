a6266ab27c1f9610641b4f82d4d723aaae5f0368
ChrisRDavila <crossdavila@gmail.com>
2024-02-05 20:07:28 -0800

    add completed bool, duedate props to item, sort by next coming due date

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,15 +1,54 @@
 using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly ToDoListContext _db;
+
+      public HomeController(ToDoListContext db)
+      {
+        _db = db;
+      }
 
       [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
+    public ActionResult Index()
+    {
+      Category[] cats = _db.Categories.ToArray();
+      Item[] items = _db.Items.ToArray();
+      Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+      model.Add("categories", cats);
+      model.Add("items", items);
+      return View(model);
+    }
 
     }
 }
+
+// can also be written as 
+// public ActionResult Index()
+//     {
+//       Category[] cats = _db.Categories.ToArray();
+//       Item[] items = _db.Items.ToArray();
+//       Dictionary<string,object[]> model = new Dictionary<string, object[]>
+//       {
+//           { "categories", cats },
+//           { "items", items }
+//       };
+//       return View(model);
+//     }
+
+// or
+// [HttpGet("/")]
+//       public ActionResult Index()
+//       {
+//         List<Category> cats = _db.Categories.ToList();
+//         List<Item> items = _db.Items.ToList();
+//         ViewBag.categories = cats;
+//         ViewBag.items = items;
+//         return View();
+//       }
+
